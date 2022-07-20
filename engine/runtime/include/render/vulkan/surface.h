4921421bd7e9f7dcbe5fcebd230888e638c3ed50
Kurino <gejingze@163.com>
2022-07-20 09:56:24 +0800

    feat: finish surface

---
+++ engine/runtime/include/render/vulkan/surface.h
@@ -0,0 +1,23 @@
+#ifndef SURFACE_H
+#define SURFACE_H
+#include <render/vulkan/utils.h>
+namespace Kosmos::Runtime::Vulkan {
+class Surface {
+private:
+    VkSurfaceKHR m_surface{VK_NULL_HANDLE};
+    VkInstance m_instanceRef{VK_NULL_HANDLE};
+
+public:
+    Surface(VkInstance instance, GLFWwindow* window);
+    Surface(Surface&& surface);
+    Surface& operator=(Surface&& surface);
+    ~Surface();
+    VkSurfaceKHR getSurace() { return m_surface; }
+
+private:
+    Surface(const Surface& surface) = delete;
+    Surface& operator=(const Surface& surface) = delete;
+};
+} // namespace Kosmos::Runtime::Vulkan
+
+#endif // SURFACE_H
