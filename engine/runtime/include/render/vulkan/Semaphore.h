0e72e374c099bd027bf28cc5476c4dd9e408d288
Kurino <gejingze@163.com>
2022-07-21 20:36:47 +0800

    feat: finish semphore wrapper

---
+++ engine/runtime/include/render/vulkan/Semaphore.h
@@ -0,0 +1,24 @@
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+#include <render/vulkan/utils.h>
+namespace Kosmos::Runtime::Vulkan {
+class Device;
+class Semaphore {
+private:
+    const Device& m_deviceRef;
+    VkSemaphore m_semaphore;
+    std::string m_name;
+
+public:
+    Semaphore(const Device& device, std::string& name);
+    Semaphore(Semaphore&& semahore);
+    ~Semaphore();
+    VkSemaphore getSemaphore() const { return m_semaphore; }
+
+private:
+    Semaphore(const Semaphore& semaphore) = delete;
+    Semaphore& operator=(const Semaphore& semaphore) = delete;
+    Semaphore& operator=(Semaphore&& semaphore) = delete;
+};
+} // namespace Kosmos::Runtime::Vulkan
+#endif // SEMAPHORE_H
---
+++ engine/runtime/src/render/vulkan/semaphore.cpp
@@ -0,0 +1,19 @@
+#include <render/vulkan/semaphore.h>
+#include <render/vulkan/device.h>
+using namespace Kosmos::Runtime::Vulkan;
+
+Semaphore::Semaphore(const Device& device, std::string& name) :
+    m_deviceRef(device), m_name(name) {
+    auto createInfo = makeInfo<VkSemaphoreCreateInfo>();
+    m_deviceRef.createSemahore(createInfo, &m_semaphore);
+}
+Semaphore::Semaphore(Semaphore&& semahore) :
+    m_deviceRef(semahore.m_deviceRef) {
+    m_semaphore = std::exchange(m_semaphore, nullptr);
+    m_name = std::move(semahore.m_name);
+}
+
+Semaphore::~Semaphore() {
+    vkDestroySemaphore(m_deviceRef.getLogicalDevice(), m_semaphore, nullptr);
+    KS_ENGINE_LOG_TRACE("Semaphore has destoryed.");
+}
